6229a214dc0162f8294c280cb514e8e486036417
David Gomesh <davidgomesh2016@gmail.com>
2020-05-21 22:10:34 -0300

    Menu principal criado
    Criei o menu principal e percebi que nao da pra trabalhar sem as Branches.

---
+++ Primeiro teste/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    printf("Olá mundo!\n");
+    printf("nina esteve aqui!";
+    printf("Marcelo Aqui");
+    printf(" astalavista baby");
+    return 0;
+}
--- agenda-telefonica/main.c
+++ agenda-telefonica/main.c
@@ -1,9 +1,45 @@
 #include <stdio.h>
 
 int main(){
-    printf("Olá mundo!\n");
-    printf ("nina esteve aqui!";
-    printf("Marcelo Aqui");
-    printf(" astalavista baby");
+    int opcao;
+
+    do{
+        printf("=======AGENDA=======\n");
+        printf("1 - Cadastrar\n");
+        printf("2 - Localizar\n");
+        printf("3 - Listar Todos\n");
+        printf("4 - Alterar\n");
+        printf("5 - Excluir\n");
+        printf("0 - Sair\n");
+        printf("OPCAO: ");
+        scanf("%i", &opcao);
+
+        switch(opcao){
+            case 1:
+                printf("CADASTAR\n");
+                break;
+            case 2:
+                printf("LOCALIZAR\n");
+                break;
+            case 3:
+                printf("LISTAR TODOS\n");
+                break;
+            case 4:
+                printf("ALTERAR\n");
+                break;
+            case 5:
+                printf("EXCLUIR\n");
+                break;
+            case 0:
+                printf("Obrigado por usar nosso programa!\n");
+                break;
+            default:
+                printf("Opcao invalida!\n");
+
+        }
+        system("pause");
+        system("cls");
+    }while(opcao!=0);
+
     return 0;
 }
